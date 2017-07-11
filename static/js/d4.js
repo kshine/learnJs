4573a438e4ad930711e5cfe198075a7d10d0459b
kshine <250270704@qq.com>
2017-07-11 13:33:56 +0800

    typeof

--- static/js/d4.js
+++ static/js/d4.js
@@ -1,7 +1,39 @@
 /**
  * Created by kshine on 2017/7/10.
  */
+/*
+ var box;
+ alert(box);
+ alert(typeof box); //box是Undefined类型 值是undefined 类型返回的字符串undefined
 
-var box = 100;
+ var box =true;
+ alert(box);
+ alert(typeof box); //box是Boolean类型 值是true 类型返回的字符串boolean
+
+ var box ='王大锤';
+ alert(box);
+ alert(typeof box); //box是String类型 值是'王大锤' 类型返回的字符串string
+
+ var box =250;
+ alert(box);
+ alert(typeof box); //box是Number类型 值是250 类型返回的字符串number
+
+ //空的对象，表示这个对象创建了，里面没东西
+ var box ={};  //new Object();
+ alert(box);
+ alert(typeof box); //box是Object类型 值是[object Object] 类型返回的字符串object
+ [object objectname] 小写的是方法返回的值的格式中默认的，大些的是对象的名字Object
+
+ //空对象，表示没有创建，就是一个null
+ var box =null;
+ alert(box);
+ alert(typeof box); //box是Null类型 值是null 类型返回的字符串object
+
+ function box(){};
+ alert(box);
+ alert(typeof box); //box是Function函数 值是function box(){}  类型返回的字符串function
+
+ */
+function box(){};
 alert(box);
-alert(typeof box);
+alert(typeof box); //box是Function函数 值是function box(){}  类型返回的字符串function
