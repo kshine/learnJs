41d770c4bfecab8000ef395b07fd6509c9f2d461
kshine <250270704@qq.com>
2017-07-16 20:15:56 +0800

    函数

---
+++ static/js/d7.js
@@ -0,0 +1,21 @@
+/**
+ * Created by kshine on 2017/7/16.
+ */
+
+function box(){
+    alert('Hello world');
+}
+
+//box();
+
+function info(name,age){
+    return '你的姓名是：'+name+'，你的年龄是：'+age;
+}
+
+// //直接调用
+// alert(info('王大锤',26));
+// //接收返回值，然后alert
+// var personinfo = info('王大锤',26);
+// alert(personinfo);
+
+//return 退出当前执行函数
